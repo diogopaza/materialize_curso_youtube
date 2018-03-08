3122500f7ae9d0bb7afc6baaba8cb4ebe982bf00
Diogo Paza <diogopazacvel@gmail.com>
2018-03-08 11:38:29 -0300

    site pequisas inicio

---
+++ js/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+	 $('#modal1').modal();
+	 $('#btn-dropdown-perfil').dropdown({
+
+
+	 });
+
+})
+
+function callParallax(){
+
+	$('.parallax').parallax();
+}
+
+window.load = callParallax();
